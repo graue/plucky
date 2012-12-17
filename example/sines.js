f6d8a916812a42f92ec0fce65429e7ba5ff679e9
James Halliday <mail@substack.net>
2012-12-17 00:06:43 -0800

    dox

--- example/sines.js
+++ example/sines.js
@@ -10,14 +10,12 @@ var music = [
     },
     function (t, clip) {
         if (t > 1 / 4) clip.end();
-        return Math.sin(tau * t * 200 * Math.pow(2, 1/3))
-            + Math.sin(tau * t * 240 * Math.pow(2, 1/3))
-        ;
+        return Math.sin(tau * t * 200 * Math.pow(2, 1/3));
     },
     function (t, clip) {
         if (t > 1 / 2) clip.go(0);
-        return Math.sin(tau * 80 * t)
-            + Math.sin(tau * 85 * t)
+        return Math.sin(tau * t * 100)
+            + Math.sin(tau * t * 105) / 8
         ;
     }
 ];
